9d6365ab124065b8b1bd14af86ac7022f0d645bc
trinehaave <trinehaave@gmail.com>
2017-11-01 15:03:24 +0100

    changes to entries-routes

--- routes/entries-routes.js
+++ routes/entries-routes.js
@@ -5,7 +5,25 @@ const router = express.Router()
 router.route('/')
   .get((req, res) => {
     console.log('get')
-    res.send('hello entries')
+    res.send('getting entries')
+  })
+
+router.route('/')
+  .post((req, res) => {
+    console.log('post')
+    res.send('posting entries')
+  })
+
+router.route('/')
+  .put((req, res) => {
+    console.log('put')
+    res.send('changing/putting entries')
+  })
+
+router.route('/')
+  .delete((req, res) => {
+    console.log('delete')
+    res.send('deleting entries')
   })
 
 module.exports = router
